d7d0de755f26f28f83721d7533e9c3b00e0f95a4
Robert Gutsch <einsezweie@gmx.de>
2021-11-25 17:50:00 +0100

    added params.py, put the data getter in one file and in one calling structure, updated requirements

---
+++ crypto_prediction/params.py
@@ -0,0 +1,21 @@
+# translate table for coins in their respective name
+
+COIN_TRANSLATION_TABLE = {
+    'doge' : {
+        'trend': 'dogecoin',
+        'coingecko': 'dogecoin',
+        'display': 'Doge'
+    },
+    'shiba-inu' : {
+        'trend': 'shiba-inu coin',
+        'coingecko': 'shiba-inu',
+        'display': 'Shiba-Inu'
+    },
+    'samoyed' : {
+        'trend': 'samoyedcoin',
+        'coingecko': 'samoyecdoin',
+        'display': 'Samoyed'
+    }
+
+
+}
