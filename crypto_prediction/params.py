61f24d4d7486e0132ab7e4a520871a99af676e45
Robert Gutsch <einsezweie@gmx.de>
2021-11-29 16:00:51 +0100

    finished prediction df

---
+++ crypto_prediction/params.py
@@ -0,0 +1,102 @@
+COIN_TRANSLATION_TABLE = {
+    'doge': { # danger: != Binance-Peg Dogecoin (DOGE)
+        'trend': 'dogecoin',
+        'id_coingecko': 'dogecoin',
+        'display': 'Doge'
+    },
+    'shib': {
+        'trend': 'shiba-inu coin',
+        'id_coingecko': 'shiba-inu',
+        'display': 'Shiba-Inu'
+    },
+    'samo': {
+        'trend': 'samoyedcoin',
+        'id_coingecko': 'samoyedcoin',
+        'display': 'Samoyed'
+    },
+    'elon': {
+        'trend': 'dogelon mars',
+        'id_coingecko': 'dogelon-mars',
+        'display': 'Dogelon Mars'
+    },
+    'hoge': {
+        'trend': 'hoge finance',
+        'id_coingecko': 'hoge-finance',
+        'display': 'Hoge Finance'
+    },
+    'mona': {
+        'trend': 'monacoin',
+        'id_coingecko': 'monacoin',
+        'display': 'MonaCoin'
+    },
+    'dogedash': {
+        'trend': 'dogedash',
+        'id_coingecko': 'doge-dash',
+        'display': 'Doge Dash'
+    },
+    'erc20': {
+        'trend': 'erc20 coin',
+        'id_coingecko': 'erc20',
+        'display': 'ERC20'
+    },
+    'ban': {
+        'trend': 'banano',
+        'id_coingecko': 'banano',
+        'display': 'Banano'
+    },
+    'cummies': {
+        'trend': 'cumrocket',
+        'id_coingecko': 'cumrocket',
+        'display': 'CumRocket'
+    },
+    'doggy': {
+        'trend': 'doggy coin',
+        'id_coingecko': 'doggy',
+        'display': 'Doggy'
+    },
+    'smi': {
+        'trend': 'safemoon inu',
+        'id_coingecko': 'safemoon',
+        'display': 'SafeMoon Inu'
+    },
+    'doe': {
+        'trend': 'dogs of elon coin',
+        'id_coingecko': 'dogsofelon',
+        'display': 'Dogs of Elon'
+    },
+    'pepecash': {
+        'trend': 'pepecash',
+        'id_coingecko': 'pepecash',
+        'display': 'Pepecash'
+    },
+    'wow': {
+        'trend': 'wownero',
+        'id_coingecko': 'wownero',
+        'display': 'Wownero'
+    },
+    'dinu': {
+        'trend': 'dogey-inu',
+        'id_coingecko': 'dogey-inu',
+        'display': 'Dogey-Inu'
+    },
+    'yummy': {
+        'trend': 'yummy coin',
+        'id_coingecko': 'yummy',
+        'display': 'Yummy'
+    },
+    'shibx': {
+        'trend': 'shibavax',
+        'id_coingecko': 'shibavax',
+        'display': 'Shibavax'
+    },
+    'lowb': {
+        'trend': 'losercoin',
+        'id_coingecko': 'loser-coin',
+        'display': 'Loser Coin'
+    },
+    'grlc': {
+        'trend': 'garlicoin',
+        'id_coingecko': 'garlicoin',
+        'display': 'Garlicoin'
+    }
+}
